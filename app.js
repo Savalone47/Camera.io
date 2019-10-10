ffa7e609a22a2741ba1733d79389c40ca391ee8e
savalone47 <savalonescott47@gmail.com>
2019-10-10 13:27:07 +0200

    main function of socket

--- app.js
+++ app.js
@@ -2,14 +2,25 @@ const express = require("express"),
       app = new express(),
       http = require("http").Server(app),
       io = require("socket.io")(http),
+      path = require('path'),
+      favicon = require('serve-favicon'),
       port  = process.env.PORT || 3000;
       
-
+// midleware
+app.use(favicon(path.join(__dirname + '/public','favicon.ico')));
 app.use(express.static(__dirname + '/public'));
 app.get('/',(req, res)=>{
     res.redirect('index.html');
 });
 
-app.listen(port, function(){
+// Socket.io
+io.on('connection',(socket)=>{
+    socket.on('stream',(image)=>{
+        socket.broadcast.emit('stream', image);
+    });
+});
+
+// Run the Server
+http.listen(port, function(){
     console.log("Server is running on port %s ", port);    
 });
